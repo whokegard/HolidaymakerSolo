c2e177e0c0fc43762f0e845608d00324bd1a71b5
whokegard <rewelox@hotmail.com>
2022-01-09 15:10:18 +0100

    Added if statement in add booking method.

--- src/Booking.java
+++ src/Booking.java
@@ -1,5 +1,12 @@
+import javax.print.attribute.standard.DialogOwner;
+import java.awt.geom.NoninvertibleTransformException;
+
 public class Booking {
     private Database database = new Database();
+    private String meal;
+    private String bed;
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_RESET = "\u001B[0m";
 
     public void searchBooking(){
         try{
@@ -22,8 +29,28 @@ public class Booking {
                 int room_id = Dialog.dialog("Room ID?");
                 database.addBookingRoom(room_id, checkin_date, checkout_date);
 
-                String meal = Dialog.dialogString("Meal choice? (None/Half board/Full board)");
-                String bed = Dialog.dialogString("Additional bed? (Yes/No)");
+                int mealChoice = Dialog.dialog("Meal choice?" + "\n" +
+                        "(1) None." + "\n" +
+                        "(2) Half board." + "\n" +
+                        "(3) Full board." + "\n", 1, 3);
+                if(mealChoice == 1){
+                    meal = "None";
+                }
+                if(mealChoice == 2){
+                meal = "Half board";
+                }
+                if(mealChoice == 3){
+                meal = "Full board";
+                }
+            int becChoice = Dialog.dialog("Bed choice?" + "\n" +
+                    "(1) Yes." + "\n" +
+                    "(2) No." + "\n", 1, 2);
+            if(becChoice == 1){
+                bed = "Yes";
+            }
+            if(becChoice == 2){
+                bed = "No";
+            }
                 int booked_id = Dialog.dialog("Book id?");
                 database.addAdditionalChoices(room_id, booked_id, meal, bed);
 
@@ -32,6 +59,7 @@ public class Booking {
                     int guestID = Dialog.dialog("Guest id?");
                     int choice_id = Dialog.dialog("Choice id?");
                     database.guestBooking(guestID, room_id, choice_id, booked_id, totalGuests);
+                    System.out.println(TEXT_GREEN + "Booking added." + TEXT_RESET);
                 }
         }
         catch(Exception e) {
